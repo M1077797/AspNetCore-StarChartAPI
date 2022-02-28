b306fa500101e023cca235c40675b31d8eab675f
M1077797 <M1077797@mindtree.com>
2022-02-28 12:50:57 +0000

    Complete module

---
+++ StarChart/Controllers/CalestialObjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CalestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CalestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
